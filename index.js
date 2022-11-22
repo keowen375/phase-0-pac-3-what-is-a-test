974c2a16f5daa019439a2050a3f26e2806c2421d
keowen375@gmail.com <keowen375@gmail.com>
2022-11-22 15:45:37 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -6,26 +6,10 @@ module.exports = {name, height, message};
 console.log(message);
 const { name, height, message } = require("../index.js");
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+const name = "Joe";
+const height = 74;
+const name = "Susan";
+const height = 35;
+const message = `${name} is ${height} inches tall`;
 
-
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
-    })
-  })
-})
-*/
+module.exports = {name, height, message};
